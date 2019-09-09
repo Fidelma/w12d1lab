089ddbb60cbe64bfcff107ab58a04fae5afa37b0
Josh Gibson <flash50k@gmail.com>
2019-09-09 13:03:12 +0100

    employee abstract

---
+++ src/main/java/staff/Employee.java
@@ -0,0 +1,33 @@
+package staff;
+
+public abstract class Employee {
+
+   private String name;
+   private String nationalInsuranceNumber;
+   private double salary;
+
+   public Employee(String name, String nationalInsuranceNumber, double salary){
+       this.name = name;
+       this.nationalInsuranceNumber = nationalInsuranceNumber;
+       this.salary = salary;
+   }
+    public String getName() {
+        return name;
+    }
+
+    public String getNationalInsuranceNumber() {
+        return nationalInsuranceNumber;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void raiseSalary(double amount){
+       this.salary += amount;
+    }
+
+    public double payBonus(){
+       return this.salary * 0.01;
+    }
+}
